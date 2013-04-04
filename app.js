6a66329480a62af9595e932867011a5b95c4f53a
Narciso Jaramillo <njaramil@adobe.com>
2013-04-03 18:10:44 -0700

    Add Brackets favicon

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.configure(function () {
     app.set("views", path.resolve(__dirname, "views"));
     app.set("view engine", "html");
     app.engine("html", require("hbs").__express);
+    app.use(express.favicon(path.resolve(__dirname, "public/favicon.ico")));
     app.use(express.logger("dev"));
     app.use(express.cookieParser());
     app.use(express.bodyParser());
